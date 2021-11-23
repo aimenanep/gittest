dc80c1ac5ce054b5663cb1e8aa0eb7cb85e5ef1c
Aimen <aimen.anep@gmail.com>
2021-11-23 13:46:03 +0100

    added postss

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+
+
+class PostTest(TestCase):
+
+    def setUp(self):
+        self.post=Post.objects.create(title="first title",slug="first-title",description="first description")
+        print("Setup testing posts.....")
+
+
+    def test_post_model(self):
+        d=self.post
+        print("testing post model .....")
+        print(f"the post  {d.title} {d.slug} {d.description}" )
+
+
+        self.assertTrue(isinstance(d,Post)) 
+
+        print(self.assertEqual(d.title,"first tie"))    
